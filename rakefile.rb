81e70321ee630c19ca8fd6e0b47bd2ce9c270506
Edward Hiley <edwardhiley@edward-hileys-macbook.local>
2011-01-27 22:27:51 +0000

    latest changes for import process

--- rakefile.rb
+++ rakefile.rb
@@ -15,6 +15,11 @@ require 'lib/classify'
 
 require 'curb'
 
+## TODO
+# need to try catorgorise the urls...
+# output will look like:
+# article url, mapped terms, autocat terms (relevancy...)
+
 @mappings
 @csv_paths
 @specialism_mapping
@@ -32,12 +37,6 @@ task :generate_csv_paths do
   @csv_paths
 end
 
-task :classify_url => [:generate_csv_paths] do
-
-classify_url "http://www.thecochranelibrary.com/details/collection/978807/Cochrane-Influenza-Resources-evidence-from-Cochrane-Reviews.html"
-  
-end
-
 task :find_ontology_ids => [:generate_csv_paths] do
 
   @known_paths = Hash.new
@@ -51,17 +50,22 @@ task :find_ontology_ids => [:generate_csv_paths] do
       # is not the head row and is not excluded from processing
       if row[0] != "SC_TERM" and row[3] != "1"
         
-        term = row[1]
+        term = row[1].to_s.strip
 
         if @known_paths[term].nil?
           url = "http://80.71.2.9/ses?TBDB=disp_taxonomy&TEMPLATE=service.json&SERVICE=term&TERM=#{URI.escape(term)}"
           puts "retrieving #{url} ..."
 
-          f = open(url)
-          raw_json = f.read
-          data = JSON.parse(raw_json)
+          begin
+            f = open(url)
+            raw_json = f.read
+            data = JSON.parse(raw_json)
+          rescue StandardError => boom
+            p boom
+            data = nil
+          end
           
-          if data["terms"].nil?
+          if data.nil? or data["terms"].nil?
             @known_paths[term] = {
               :path => [],
               :ontology_id => 0
@@ -84,32 +88,21 @@ task :find_ontology_ids => [:generate_csv_paths] do
           "MESH_TERM" => row[1],
           "MESH_ID" => row[2],
           "IGNORE" => 0, # excluding ignored rows already
-          "PATH" => term_info[:paths],
-          "ONTOLOGY_ID" => term_info[:ontology_id]
+          "ONTOLOGY_ID" => term_info[:ontology_id],
+          "PATH" => term_info[:paths].to_json
         }
         
       end
-
-      
     end
-    
     
     CSV.open("#{@mapping_data_folder}/#{File.basename(path, ".csv")}_converted.csv", "wb") do |csv|
       @new_mappings.each{|m|
-        csv << [m["SC_TERM"], m["MESH_TERM"], m["MESH_ID"], m["IGNORE"], m["PATH"], m["ONTOLOGY_ID"]]
+        csv << [m["SC_TERM"], m["MESH_TERM"], m["MESH_ID"], m["IGNORE"], m["ONTOLOGY_ID"], m["PATH"]]
       }
     end
-    
   } 
   
-    
 end
-
-
-# yield like processing for csv to simplify...
-# need to try catorgorise the urls...
-# output will look like:
-# article url, mapped terms, autocat terms (relevancy...)
 
 task :generate_mappings => [:generate_csv_paths] do
   
@@ -127,7 +120,8 @@ task :generate_mappings => [:generate_csv_paths] do
       
       # is not the head row and is not exluded from processing
       if row[0] != "TOPIC" and row[3] != "1"
-        specialism.add(row[0], row[1], row[2], row[4], row[5])
+        term_path = JSON.parse(row[5]) if row[5] != "null"
+        specialism.add(row[0], row[1], row[2], row[4], term_path)
       end
       
     end
@@ -140,10 +134,6 @@ task :generate_mappings => [:generate_csv_paths] do
   puts "processed mappings written to mappings.yaml"
   
 end
-
-# need to call service
-
-
 
 task :default => [:generate_mappings] do
   
@@ -163,7 +153,7 @@ task :default => [:generate_mappings] do
                 
         puts "Processing #{output_folder}"
         puts "#{@specialism_mapping.count} mappings found" unless @specialism_mapping.nil?
-        puts "No mappings found for #{file}, please create a CSV file with SC_TERM,MESH_TERM,MESH_ID,IGNORE columns" if @specialism_mapping.nil?
+        puts "No mappings found for #{file}, please create a CSV file named #{file}.csv with SC_TERM,MESH_TERM,MESH_ID,IGNORE columns" if @specialism_mapping.nil?
         
         
         FileUtils.mkdir_p output_folder
@@ -194,15 +184,15 @@ def create_document (doc, area_of_interest)
     document['RMSId'] = doc['id'].to_s
     document['RMSName'] = doc['name'].to_s.strip_cdata.strip
     document['RMSRootDirectory'] = doc['rootDirectory'].to_s.strip_cdata
-    document['RMSLastReviewDate'] = doc['lastReviewDate'].to_s.to_date
+    #document['RMSLastReviewDate'] = doc['lastReviewDate'].to_s.to_date
     document['RMSKeywords'] = keywords
 
     document['RelatedUrls'] = [] # seek origin
     document['TargetCountry'] = ""
     document['Creator'] = "RMS Import"
-    document['ResourceType'] = doc['resourceType'].to_s
+    #document['ResourceType'] = doc['resourceType'].to_s
     document['Title'] = doc['title'].to_s
-    document['TitleForSorting'] = document['Title'].to_sort
+    #document['TitleForSorting'] = document['Title'].to_sort
     document['Description'] = Sanitize.clean(doc['body'].to_s.strip_cdata, Sanitize::Config::RELAXED)
     document['Url'] = doc['url'].to_s.strip_cdata
     document['Attachment'] = ""
@@ -212,7 +202,7 @@ def create_document (doc, area_of_interest)
     document['Audience'] = audience[0].to_audiences rescue []
     
     document['PublicationDate'] = doc['publicationDate'].to_s.to_date
-    document['ReviewDate']  # seek origin
+    #document['ReviewDate']  # seek origin
     document['Source']  = area_of_interest
     document['Publisher'] = doc['publisher'].to_s
     document['Contributor'] = "RMS Import"
@@ -220,11 +210,7 @@ def create_document (doc, area_of_interest)
     
     document['Tags'] = map_keywords(keywords.split(",")) unless @specialism_mapping.nil?
     document['AreaOfInterest'] = area_of_interest
-    document['CreatedDate'] = Time.now.to_s
-    
-    # dates = /Date([number])/ - unable to process to this, (to_i) needs (1.9.x)
-    # amanda cambell, fran, about about audience and relatedUrls
-    # use womenshealth, put through autotagger...
+    document['CreatedDate'] = Time.now.to_s.to_date
     
     document
 end
@@ -252,8 +238,8 @@ def map_keywords(keywords)
   # find matching paths, need to add lookup to real terms in ontology (using service)?
 end
 
-
 def process_term(data)
+  
   term_data = data["terms"][0]["term"]
 
   term_paths = term_data["paths"]
@@ -261,17 +247,13 @@ def process_term(data)
   mapped_paths = []
   
   term_paths.each{|paths|
-    mapped_paths << "/" + paths["path"].collect{|path| path["field"]["id"] }.join('/') + "/"
+    mapped_paths << "|" + paths["path"].collect{|path| path["field"]["name"] }.join('|') + "|"
   }
   
   term_info = {
     :paths => mapped_paths,
     :ontology_id => term_data["id"]
   }
-end
-
-def get_paths
-  
 end
 
 class String
@@ -281,12 +263,11 @@ class String
   end
   
   def to_date
-    Date.parse(self) rescue nil
+    Date.parse(self).strftime('%Y-%m-%dT%H:%M:%S%z') rescue "" #"that's a no-no #{self}"
   end
   
   def to_sort
     self.gsub(/[^a-z0-9]+/i, '').downcase
-    #self.gsub(/[^\x20-\x7E]/, '').downcase
   end
   def strip_cdata
     self.gsub('<![CDATA[','').gsub(']]>','')
